891ab00f80e83ef5316ce26b8dedaa9dd00a2da7
Samuel Milhomens Martins <samuel.mil.mar@gmail.com>
2024-09-29 14:23:13 -0300

    First commit

---
+++ src/main/java/com/br/samuel/controllers/PersonController.java
@@ -0,0 +1,10 @@
+package com.br.samuel.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+	
+}
---
+++ src/main/java/com/br/samuel/services/PersonService.java
@@ -0,0 +1,9 @@
+package com.br.samuel.services;
+
+public class PersonService {
+
+	public PersonService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
